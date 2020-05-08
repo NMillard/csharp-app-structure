f3f6e643dc32cf1063b1ee3941a0972162f57c01
nmillard <n.millard@me.com>
2020-05-08 12:58:50 +0200

    initial

---
+++ BookStore.WebClient/Controllers/AuthorsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.WebClient.Controllers {
+    
+    [Route("api/[controller]/[action]")]
+    public class AuthorsController : ControllerBase {
+        public AuthorsController() {
+        }
+    }
+}
